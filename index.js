0580f771c18dd59688c93398df6ff571b90eba18
chrisvasqm <christianvasquezmelo@gmail.com>
2024-05-17 21:15:42 -0400

    Add catch all route

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const app = express();
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
+app.use((req, res) => {
+    res.status(404).send('Not found');
+})
 
 const port = process.env.PORT || 3030;
 
