02835f7cf513e9a8963fd8c665eeec71125c5061
chrisvasqm <christianvasquezmelo@gmail.com>
2024-05-17 20:49:42 -0400

    Run migrations programatically

--- data/database.js
+++ data/database.js
@@ -3,4 +3,17 @@ const config = require("./knexfile");
 
 const database = knex(config);
 
+const runMigrations = async () => {
+    try {
+        await database.migrate.latest();
+        console.log('Migrations have run successfully');
+    } catch (error) {
+        console.error('Error running migrations:', error);
+    } finally {
+        database.destroy();
+    }
+}
+
+runMigrations();
+
 module.exports = database;
