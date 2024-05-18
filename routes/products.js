d7222e79453cf2ab7a18b9f85a72bf5d76269a9e
chrisvasqm <christianvasquezmelo@gmail.com>
2024-05-17 21:51:08 -0400

    Add create product API

--- routes/products.js
+++ routes/products.js
@@ -1,12 +1,33 @@
-const { Router } = require('express');
 const database = require('../data/database');
+const { Router } = require('express');
+const { z } = require('zod');
+
+const schema = z.object({
+    name: z.string({ required_error: 'Name is required' }).min(1, 'Name must be at least 1 character long'),
+    price: z.number({ required_error: 'Price is required' }).positive('Price must be greater than 0'),
+    quantity: z.number({ required_error: 'Quantity is required' }).positive('Quantity must be greater than 0')
+})
 
 const router = Router();
 
-router.get('/products', (_, res) => {
+router.get('/products', (_, response) => {
     database('products')
         .then(products => res.send(products))
         .catch(_ => res.status(500).send('Failed to fetch products'));
+});
+
+router.post('/products', (request, response) => {
+    const validation = schema.safeParse(request.body);
+    if (!validation.success)
+        return response.status(400).send(validation.error.format());
+
+    const { name, price, quantity } = request.body;
+
+    database('products')
+        .insert({ name, price, quantity })
+        .returning('*')
+        .then(product => response.send(product))
+        .catch(_ => response.status(500).send('Failed to save new product'));
 });
 
 module.exports = router;
