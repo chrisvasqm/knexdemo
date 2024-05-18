af19511274dd4c28701dbf1da29ec70c554aa267
chrisvasqm <christianvasquezmelo@gmail.com>
2024-05-17 21:27:38 -0400

    Remove destroy call to fix /products API

--- data/database.js
+++ data/database.js
@@ -3,17 +3,8 @@ const config = require("./knexfile");
 
 const database = knex(config);
 
-const runMigrations = async () => {
-    try {
-        await database.migrate.latest();
-        console.log('Migrations have run successfully');
-    } catch (error) {
-        console.error('Error running migrations:', error);
-    } finally {
-        database.destroy();
-    }
-}
-
-runMigrations();
+database.migrate.latest()
+    .then(() => console.log('Migrations have run successfully'))
+    .catch(error => console.error('Error running migrations:', error))
 
 module.exports = database;
