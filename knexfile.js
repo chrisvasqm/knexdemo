c34eb4f38c3e2ba23d0746b3b58307440eb35dd5
chrisvasqm <christianvasquezmelo@gmail.com>
2024-05-17 22:38:04 -0400

    Knex init

---
+++ knexfile.js
@@ -0,0 +1,20 @@
+const config = {
+  client: process.env.DATABASE_CLIENT,
+  connection: {
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations',
+    directory: __dirname + '/migrations'
+  },
+}
+
+module.exports = config;
--- routes/products.js
+++ routes/products.js
@@ -1,4 +1,4 @@
-const db = require('../database/db');
+const db = require('../db');
 const { Router } = require('express');
 const { z } = require('zod');
 
