7056996df6c14d7956f661f2c7817d3646e6e4d6
chrisvasqm <christianvasquezmelo@gmail.com>
2024-05-17 21:15:29 -0400

    Setup express server

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello Knex');
+const database = require('./data/database');
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+const port = process.env.PORT || 3030;
+
+app.listen(port, () => console.log(`Server running on http://localhost:${port}`))
