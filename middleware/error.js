dba82a2ff2e8fc0e9d9dad00f1006ed77ace9af2
chrisvasqm <christianvasquezmelo@gmail.com>
2024-05-19 08:52:12 -0400

    Make 500 error message friendlier

--- middleware/error.js
+++ middleware/error.js
@@ -3,7 +3,7 @@ const logger = require('../logger');
 const error = (err, req, res, next) => {
     logger.error(err.message, err);
 
-    res.status(500).send('Internal server error');
+    res.status(500).send('Something went wrong. Please try again later.');
 };
 
 module.exports = error;
